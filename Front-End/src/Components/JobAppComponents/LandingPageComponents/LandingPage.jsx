848610e91aba6e90f83b908d219e0b373042560b
A4-emrys <64101335+A4-emrys@users.noreply.github.com>
2022-04-20 14:47:35 +0100

    Contains the whole landing page

---
+++ Front-End/src/Components/JobAppComponents/LandingPageComponents/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../HeaderComponents/Header';
+import Banner from '../BannnerComponents/Banner';
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  )
+}
+
+export default LandingPage;
