e044448724dede594cd4cafbe9d0753fcb7825a5
A4-emrys <64101335+A4-emrys@users.noreply.github.com>
2022-04-20 15:01:09 +0100

    Add files via upload

---
+++ Front-End/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import JobApp from './Components/JobAppComponents/JobApp';
+
+
+
+function App() {
+  return (
+    <div>
+        <JobApp/>
+    </div>
+  );
+}
+
+export default App;
